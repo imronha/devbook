3bb547a4116906902012a8a486ca6c49e00b45c8
Imron Hajiahmad <imronha@gmail.com>
2020-04-04 16:22:36 -0700

    server done, mongodb connected, rester user complete

---
+++ config/db.js
@@ -0,0 +1,22 @@
+// Connection logic
+
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+// Connect to mongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    // Log error message and exit with failure
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ routes/api/users.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+// Express validator
+const { check, validationResult } = require("express-validator");
+
+// Import user models
+const User = require("../../models/User");
+
+// =================================================================
+
+// @route   POST api/users
+// @desc    Register new user
+// @access  Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please use a valid email").isEmail(),
+    check("password", "Password must be 6 or more characters long").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    // console.log(res);
+    // Handle validation response & errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    // console.log(req.body);
+
+    try {
+      // console.log("start try block, try to find", email);
+      // // Check if user already exists
+      let user = await User.findOne({ email });
+      // console.log("in the try block after findone");
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm", // Default
+      });
+
+      // Create new instance of user
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //   Encrypt password and then save user
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // Return jsonwebtokens
+      res.send("User Registered");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
