400095fc3a22159de2a695728da1f78d56c8e494
Imron Hajiahmad <imronha@gmail.com>
2020-04-08 18:20:35 -0700

    created privateroute for dashboard

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+// Main dashboard component
+// Fetch all data using actions and pass down to other components
+import React from "react";
+import PropTypes from "prop-types";
+
+const Dashboard = (props) => {
+  return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
