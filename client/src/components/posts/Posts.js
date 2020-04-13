eb8464dd34fa878582410271fa19c9759c964ecd
Imron Hajiahmad <imronha@gmail.com>
2020-04-12 17:30:58 -0700

    delete post, add post completed

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -6,6 +6,7 @@ import Spinner from "../layout/Spinner";
 import { connect } from "react-redux";
 import { getPosts } from "../../actions/post";
 import PostItem from "./PostItem";
+import PostForm from "./PostForm";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -20,7 +21,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
       <p className="lead">
         <i className="fas fa-user">Welcome to the community</i>
       </p>
-      {/* Postform */}
+      <PostForm />
       <div className="posts">
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
