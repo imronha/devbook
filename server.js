20aff3c7589099e3bf205bac674bb0a9e9980ca8
Imron Hajiahmad <imronha@gmail.com>
2020-04-13 14:52:54 -0700

    prep for deployment

--- server.js
+++ server.js
@@ -19,12 +19,11 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 
-// Serve static assests in production
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
-  //Serve index.html file
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
